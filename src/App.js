4be5fa91def3ed037b853ec40d4ed405710a91f7
Natalie Acevedo <natalieacevedo25@gmail.com>
2021-10-27 16:58:15 +0100

    was able to pass info to hijos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from "react";
+import axios from 'axios';
+import DisplayCharacter from './DisplayCharacter';
+//import DisplayCharacter from './DisplayCharacter';
+
+
 
 function App() {
+
+  
+
+  const [quote, setQuote] = React.useState("Get your Simpson's daily wisdom dose");
+
+  function getApi() {
+    axios
+      .get("https://simpsons-quotes-api.herokuapp.com/quotes")
+      .then((response) => {
+        console.log(response)
+        return response.data[0]
+      })
+      .then((data) => {
+       
+      // console.log(data)
+        //setQuote(data.quote)
+        setQuote(data);
+      });
+  
+  };
+
+
+
+  
+  console.log(quote);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>The Simpsons</h1>
+      <DisplayCharacter allInformacion={getApi} quote={quote}/>
     </div>
   );
 }
+
+
+
+
+
+
+  
+
+  
+  // const getEmployee = () => {
+  //   // Send the request
+  //   axios
+  //     .get('https://randomuser.me/api?nat=en')
+  //     // Extract the DATA from the received response
+  //     .then((response) => response.data)
+  //     // Use this data to update the state
+  //     .then((data) => {
+  //       setEmployee(data.results[0]);
+  //     });
+  // };
+
+
+  
+
+
+ 
 
 export default App;
---
+++ src/DisplayCharacter.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function DisplayCharacter({allInformacion,quote}) {
+    
+    console.log(allInformacion);
+    console.log(quote);
+    
+
+
+
+
+    return (
+        <h1>soy tu hijo</h1>
+    )
+
+
+
+};
+
+export default DisplayCharacter;
